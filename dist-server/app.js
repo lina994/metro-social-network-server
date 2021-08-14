094aafd7b45feee1dba778a157b71abadf52e7ec
g94lina <28238824+lina994@users.noreply.github.com>
2021-08-14 11:02:34 +0300

    install (express, babel, rimraf, nodemon. dotenv) and basic configuration

--- dist-server/app.js
+++ dist-server/app.js
@@ -1,41 +1,34 @@
-import dotenv from 'dotenv'
-dotenv.config()
+"use strict";
 
-import express from 'express'
+var _dotenv = _interopRequireDefault(require("dotenv"));
 
+var _express = _interopRequireDefault(require("express"));
 
-const PORT = process.env.PORT || 3000
-// const PORT = 3000
-const app = express()
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
- 
-app.listen(PORT, () =>
-  console.log(`Server listening on port ${PORT}!`)
-);
+_dotenv["default"].config();
 
-// "start": "node src/app.js",
+var PORT = process.env.PORT || 3000; // const PORT = 3000
+
+var app = (0, _express["default"])();
+app.listen(PORT, function () {
+  return console.log("Server listening on port ".concat(PORT, "!"));
+}); // "start": "node src/app.js",
 // "type": "module",
-
 // import * as http from 'http';
 // import * as url from 'url';
 // import { parse } from 'querystring';
-
 // import { usersList } from './users.js';
-
 // const hostname = '127.0.0.1';
 // const port = 3001; // 8080
 // const allow_origin = 'http://localhost:3000';
-
-
 // let users = usersList;
 // // console.log(usersList);
 // // console.log('Program run');
-
 // const server = http.createServer((req, res) => {
 //   // CORS
 //   res.setHeader("Access-Control-Allow-Origin", allow_origin);  // TODO - edit
 //   res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");  // TODO - edit
-  
 //   if (req.method == 'GET') {
 //     let urlParts = url.parse(req.url, true);
 //     // console.log(urlParts.pathname);
@@ -64,7 +57,6 @@ app.listen(PORT, () =>
 //     res.end('its server response - unsupported method');
 //   }
 // });
-
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
